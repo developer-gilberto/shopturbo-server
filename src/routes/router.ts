ac723f80be9c353c7aa5348067a304c945133bb7
Gilberto Lopes <developer.gilberto@gmail.com>
2025-05-13 19:21:52 -0300

    fix: fix http verb from signup and signin route

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,10 +1,15 @@
 import { Router, Request, Response } from 'express';
 import { signUpService } from '../services/accounts/signup';
+import { signInService } from '../services/accounts/signin';
 
 const router = Router();
 
-router.get('/accounts/signup', (req: Request, res: Response) => {
+router.post('/accounts/signup', (req: Request, res: Response) => {
     signUpService(req, res);
+});
+
+router.post('/accounts/signin', (req: Request, res: Response) => {
+    // signInService(req, res);
 });
 
 export { router };
