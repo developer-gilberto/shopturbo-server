f06e69526372093aee11c13f50d595cc55392a6e
Gilberto Lopes <developer.gilberto@gmail.com>
2025-07-02 22:40:21 -0300

    feat: check if there is already a shop stored

--- src/services/auth/api/shopee/getAccessToken.ts
+++ src/services/auth/api/shopee/getAccessToken.ts
@@ -3,88 +3,117 @@ import { generateSignature } from "./generateSignature";
 import axios from "axios";
 import { ShopRepository } from "../../../../repositories/shopRepository";
 import { ExtendedRequest } from "../../../../interfaces/usersInterfaces";
+import { UsersRepository } from "../../../../repositories/usersRepository";
 
 export async function getAccessToken(req: ExtendedRequest, res: Response) {
     try {
-        // verificar se o shop ja tem o accessToken no redis e/ou postgres
-
-        const partnerId = Number(process.env.PARTNER_ID!);
-        const path = process.env.GET_ACCESS_TOKEN_PATH!;
-        const timestamp = Math.floor(Date.now() / 1000);
-
-        const baseString = partnerId + path + timestamp;
-
-        const sign = generateSignature(baseString, process.env.PARTNER_KEY!);
-
-        const host = process.env.AUTH_PARTNER_HOST!;
-
-        const url = `${host}${path}?partner_id=${partnerId}&timestamp=${timestamp}&sign=${sign}`;
-        const encodeUrl = encodeURI(url);
-
         const { code, shop_id } = req.query;
+        // -> verificar se o shop ja tem o accessToken no redis e/ou postgres.
 
-        const response = await axios.post(encodeUrl, {
-            code: code,
-            shop_id: Number(shop_id), // loja que vai conceder a autorizacao ao app
-            partner_id: Number(partnerId), // desenvolvedor do app
-        });
-        /*
-        exemplo da response com dados fakes:
+        // usando o shop_id que vem da query da rota, verificar se ja existe um shop no db.
+        // se existir, verificar se o token ainda eh valido.
+        // senao salvar o shop no db.
+        const shopRepo = new ShopRepository();
 
-        response.data:  {
-            refresh_token: '72734a68544c54656d63664e63481234', // válido por 30 dias e só pode ser usado uma vez pelo shop_id.
-            access_token: '6850514259537a7469414c6a6971kuju', // válido por 4 horas e pode ser usado diversas vezes.
-            expire_in: 14209, // 4 horas em segundos
-            request_id: 'e3e3e7f336003e46d42f697ad63176',
-            error: '',
-            message: ''
-        }
-        */
-        if (response.statusText !== "OK") {
-            res.status(response.status).json({
-                error: true,
-                message: "It was not possible to get the Shopee API access token :(",
-            });
-            return;
-        }
+        const storedShop = await shopRepo.getShopAndTokenByUserId(Number(req.loggedUser!.id));
+        // antes de salvar, verificar se ja esta salvo no db para nao duplicar shops
 
-        const loggedUser = req.loggedUser;
+        console.log(storedShop);
 
-        const shopRepo = new ShopRepository({
-            shopId: shop_id,
-            loggedUser,
-            ...response.data,
-        });
-
-        // neste ponto eu sei:
-        // user logado,
-        // shop que autorizou
-        // shopeeAccessToken
-
-        //antes de salvar, verificar se ja esta salvo no db(colocar unique no shop da table users para nao duplicar shops)
-        const newShop = shopRepo.save();
-
-        if ((await newShop) === null) {
+        if (storedShop === false) {
             res.status(500).json({
                 error: true,
-                message: "An error occurred while trying to save the shop to the database",
+                message: "An error occurred while trying to search for the ShopWithToken in the database.",
             });
             return;
         }
 
-        // salvar o accessToken no redis
+        if (storedShop === null) {
+            const partnerId = Number(process.env.PARTNER_ID!);
+            const path = process.env.GET_ACCESS_TOKEN_PATH!;
+            const timestamp = Math.floor(Date.now() / 1000);
 
-        // -> Cada accessToken é válido por 4 horas e pode ser usado diversas vezes. No entanto, você precisa atualizar o token fazendo uma chamada de RefreshAcessToken antes que ele expire para conseguir um novo token de acesso.
-        const { refresh_token, ...accessTokenData } = response.data;
+            const baseString = partnerId + path + timestamp;
 
-        // associar o accessToken ao loggedUser
+            const sign = generateSignature(baseString, process.env.PARTNER_KEY!);
+
+            const host = process.env.AUTH_PARTNER_HOST!;
+
+            const url = `${host}${path}?partner_id=${partnerId}&timestamp=${timestamp}&sign=${sign}`;
+            const encodeUrl = encodeURI(url);
+
+            const response = await axios.post(encodeUrl, {
+                code: code,
+                shop_id: Number(shop_id), // loja que vai conceder a autorizacao ao app
+                partner_id: Number(partnerId), // desenvolvedor do app
+            });
+
+            if (response.statusText !== "OK") {
+                res.status(response.status).json({
+                    error: true,
+                    message: "It was not possible to get the Shopee API access token :(",
+                });
+                return;
+            }
+            // neste ponto eu sei:
+            // user logado,
+            // shop que autorizou,
+            // shopeeAccessToken,
+
+            const newShop = shopRepo.save({
+                shopId: Number(shop_id),
+                userId: req.loggedUser!.id,
+                accessToken: response.data.access_token,
+                refreshToken: response.data.refresh_token,
+                expireIn: response.data.expire_in,
+            });
+
+            if (newShop === null) {
+                res.status(500).json({
+                    error: true,
+                    message: "An error occurred while trying to save the shop to the database",
+                });
+                return;
+            }
+
+            // salvar o accessToken no redis
+
+            // -> Cada accessToken é válido por 4 horas e pode ser usado diversas vezes. No entanto, você precisa atualizar o token fazendo uma chamada de RefreshAcessToken antes que ele expire para conseguir um novo token de acesso.
+            const { refresh_token, ...accessTokenData } = response.data;
+
+            // associar o accessToken ao loggedUser
+
+            res.status(200).json({
+                error: false,
+                message: "Each accessToken is valid for 4 hours and can be used several times.",
+                data: accessTokenData, //nao enviar o refresh token para o frontend
+            });
+            return;
+        }
+
+        // if (storedShop) //significa que a loja ja foi salva. agora preciso verificar se o token ainda eh valido.
+
+        const expiresAt = new Date(
+            storedShop.ShopeeAccessToken!.createdAt.getTime() + storedShop.ShopeeAccessToken!.expireIn * 1000,
+        );
+
+        const validToken = new Date() < expiresAt;
+        // const validToken = new Date() < new Date("2021-01-01T00:44:59.614Z"); // <- teste com token expirado
+
+        // if (!validToken) // fazer uma request para refresh token
+
+        const accessTokenData = {
+            shopId: storedShop.ShopeeAccessToken?.shopId,
+            accessToken: storedShop.ShopeeAccessToken?.accessToken,
+            expiresAt,
+        };
 
         res.status(200).json({
             error: false,
-            message: "Each accessToken is valid for 4 hours and can be used several times.",
-            // data: accessTokenData, //nao enviar o refresh token para o frontend
-            data: response.data,
+            message: `The token will expire in: '${expiresAt}'`,
+            accessToken: accessTokenData,
         });
+        return;
     } catch (err) {
         console.error(
             "\x1b[1m\x1b[31m[ ERROR ] an error occurred while trying to get the access token: \x1b[0m\n",
