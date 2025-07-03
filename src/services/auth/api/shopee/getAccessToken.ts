4711d4b5952b08f34c8f4a346e7ab2a38a8c54d0
Gilberto Lopes <developer.gilberto@gmail.com>
2025-07-03 20:01:38 -0300

    feat: update accessToken using refreshToken

--- src/services/auth/api/shopee/getAccessToken.ts
+++ src/services/auth/api/shopee/getAccessToken.ts
@@ -1,24 +1,16 @@
-import { Request, Response } from "express";
+import { Response } from "express";
 import { generateSignature } from "./generateSignature";
 import axios from "axios";
 import { ShopRepository } from "../../../../repositories/shopRepository";
 import { ExtendedRequest } from "../../../../interfaces/usersInterfaces";
-import { UsersRepository } from "../../../../repositories/usersRepository";
 
 export async function getAccessToken(req: ExtendedRequest, res: Response) {
     try {
         const { code, shop_id } = req.query;
-        // -> verificar se o shop ja tem o accessToken no redis e/ou postgres.
-
-        // usando o shop_id que vem da query da rota, verificar se ja existe um shop no db.
-        // se existir, verificar se o token ainda eh valido.
-        // senao salvar o shop no db.
+        // -> verificar se o shop ja tem o accessToken e se o token ainda eh valido.
         const shopRepo = new ShopRepository();
 
         const storedShop = await shopRepo.getShopAndTokenByUserId(Number(req.loggedUser!.id));
-        // antes de salvar, verificar se ja esta salvo no db para nao duplicar shops
-
-        console.log(storedShop);
 
         if (storedShop === false) {
             res.status(500).json({
@@ -28,6 +20,7 @@ export async function getAccessToken(req: ExtendedRequest, res: Response) {
             return;
         }
 
+        // antes de salvar, verificar se ja esta salvo no db para nao duplicar shops
         if (storedShop === null) {
             const partnerId = Number(process.env.PARTNER_ID!);
             const path = process.env.GET_ACCESS_TOKEN_PATH!;
@@ -59,7 +52,6 @@ export async function getAccessToken(req: ExtendedRequest, res: Response) {
             // user logado,
             // shop que autorizou,
             // shopeeAccessToken,
-
             const newShop = shopRepo.save({
                 shopId: Number(shop_id),
                 userId: req.loggedUser!.id,
@@ -76,12 +68,9 @@ export async function getAccessToken(req: ExtendedRequest, res: Response) {
                 return;
             }
 
-            // salvar o accessToken no redis
-
-            // -> Cada accessToken é válido por 4 horas e pode ser usado diversas vezes. No entanto, você precisa atualizar o token fazendo uma chamada de RefreshAcessToken antes que ele expire para conseguir um novo token de acesso.
             const { refresh_token, ...accessTokenData } = response.data;
 
-            // associar o accessToken ao loggedUser
+            // enviar somente os dados shopId accessToken expiresAt para o frontend
 
             res.status(200).json({
                 error: false,
@@ -100,7 +89,43 @@ export async function getAccessToken(req: ExtendedRequest, res: Response) {
         const validToken = new Date() < expiresAt;
         // const validToken = new Date() < new Date("2021-01-01T00:44:59.614Z"); // <- teste com token expirado
 
-        // if (!validToken) // fazer uma request para refresh token
+        if (!validToken) {
+            const partnerId = Number(process.env.PARTNER_ID!);
+            const path = process.env.GET_REFRESH_TOKEN_PATH!;
+            const timestamp = Math.floor(Date.now() / 1000);
+
+            const baseString = partnerId + path + timestamp;
+
+            const sign = generateSignature(baseString, process.env.PARTNER_KEY!);
+
+            const host = process.env.AUTH_PARTNER_HOST!;
+
+            const url = `${host}${path}?partner_id=${partnerId}&timestamp=${timestamp}&sign=${sign}`;
+            const encodeUrl = encodeURI(url);
+
+            const response = await axios.post(encodeUrl, {
+                shop_id: Number(shop_id), // loja que vai conceder a autorizacao ao app
+                refresh_token: storedShop.ShopeeAccessToken?.refreshToken,
+                partner_id: Number(partnerId), // desenvolvedor do app
+            });
+
+            if (response.statusText !== "OK") {
+                res.status(response.status).json({
+                    error: true,
+                    message: "It was not possible to get the Shopee API refresh_token :(",
+                });
+                return;
+            }
+
+            // atualizar os dados das tabelas shops e tokens no db...
+
+            res.status(response.status).json({
+                error: false,
+                message: "",
+                newAccessTokenAndRefreshToken: response.data,
+            });
+            return;
+        }
 
         const accessTokenData = {
             shopId: storedShop.ShopeeAccessToken?.shopId,
@@ -111,12 +136,12 @@ export async function getAccessToken(req: ExtendedRequest, res: Response) {
         res.status(200).json({
             error: false,
             message: `The token will expire in: '${expiresAt}'`,
-            accessToken: accessTokenData,
+            accessTokenData,
         });
         return;
     } catch (err) {
         console.error(
-            "\x1b[1m\x1b[31m[ ERROR ] an error occurred while trying to get the access token: \x1b[0m\n",
+            "\x1b[1m\x1b[31m[ ERROR ] an error occurred while trying to get the access_token: \x1b[0m\n",
             err,
         );
         res.status(500).json({
