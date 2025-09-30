2e0f700ea3acccfe6c26938f5cef03bbd5f7c768
Gilberto Lopes <developer.gilberto@gmail.com>
2025-09-30 19:20:26 -0300

    fix: fix accessToken schema to require required fields and correct sizes

--- src/domains/accessToken/schemas/getTokenSchema.ts
+++ src/domains/accessToken/schemas/getTokenSchema.ts
@@ -3,10 +3,20 @@ import z from "zod";
 export function getAccessTokenSchema() {
     return z.object({
         code: z
-            .string()
-            .min(4, "'Code' must be at least 8 characters long!")
-            .max(255, "'Code' must be at most 30 characters long!"),
+            .string({
+                required_error: "'code' é obrigatório",
+                invalid_type_error: "'code' deve ser uma string",
+            })
+            .nonempty("'code' não pode ser vazio")
+            .min(8, "'code' deve ter no mínimo 8 caracteres")
+            .max(255, "'code' deve ter no máximo 255 caracteres"),
 
-        shop_id: z.string().min(1).max(255),
+        shop_id: z
+            .string({
+                required_error: "'shop_id' é obrigatório",
+                invalid_type_error: "'shop_id' deve ser uma string",
+            })
+            .nonempty("'shop_id' não pode ser vazio")
+            .max(255, "'shop_id' deve ter no máximo 255 caracteres"),
     });
 }
