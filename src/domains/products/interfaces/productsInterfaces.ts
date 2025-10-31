548547712b2e3ad06601bc2e893cd03611c4aa18
Gilberto Lopes <developer.gilberto@gmail.com>
2025-10-31 18:33:07 -0300

    fix: correcting product interfaces that were accidentally deleted

--- src/domains/products/interfaces/productsInterfaces.ts
+++ src/domains/products/interfaces/productsInterfaces.ts
@@ -1,3 +1,15 @@
+import { Request } from 'express';
+import { IJwtPayload } from '../../user/interfaces/userInterfaces';
+
+export interface IReqParams {
+    shop_id: string;
+    item_id: string;
+}
+
+export type ExtendedReq = Request<Record<keyof IReqParams, string>, {}> & {
+    loggedUser?: IJwtPayload;
+};
+
 export interface IProduct {
     id: number;
     sku: string;
